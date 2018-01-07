6a4c96714c6b6b45deac266b65f940e235b8b562
Can <cangospcstuff@outlook.com>
2018-01-07 15:13:53 +0100

    Revert "done"
    This reverts commit 9e44cdb94a9b2eeb69b5100930d58db4bc5b6521.

--- pp.a2.01-HandOverHandLocking/src/io/dama/par/hoh/ThreadsafeLinkedNewList.java
+++ pp.a2.01-HandOverHandLocking/src/io/dama/par/hoh/ThreadsafeLinkedNewList.java
@@ -1,82 +1,106 @@
 package io.dama.par.hoh;
 
 import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadsafeLinkedNewList<T> implements NewList<T> {
-	private class ListElement<T> {
-		private T element;
-		private ListElement<T> prev;
-		private final ListElement<T> next;
-		private ReentrantLock lockfirst = new ReentrantLock();
+    private class ListElement<T> {
+        private T                    element;
+        private ListElement<T>       prev;
+        private final ListElement<T> next;
+        private Lock lock;
+        
+        
+        
+        private ListElement(final T element, final ListElement<T> prev, final ListElement<T> next) {
+            this.element = element;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
 
-		private ListElement(final T element, final ListElement<T> prev, final ListElement<T> next) {
-			this.element = element;
-			this.prev = prev;
-			this.next = next;
-		}
-	}
+    private ListElement<T> first;
 
-	private ListElement<T> first;
+    public ThreadsafeLinkedNewList() {
+        this.first = null;
+    }
 
-	public ThreadsafeLinkedNewList() {
-		this.first = null;
-	}
+    @Override
+    public  T get(final int i) {
+        int j = 0;
+        ListElement<T> ptr = this.first;
+        ListElement<T> tmp = null;
+        //aktueller Node wird gelockt
+        ptr.lock.lock();
+        while (j++ < i) {
+        	
+        	try {
+        		
+        	ptr = ptr.next;
+            tmp = ptr.prev;
+            
+            //der nächste Node wird gelockt
+            ptr.lock.lock();
+        
+        	}
+        	finally {
+        		
+        	    //der vorherige Node wird geunlockt
+                tmp.lock.unlock();
+        		
+        	}        
+        }        
+        ptr.lock.unlock();
+        return ptr.element;
+    }
 
-	@Override
-	public T get(final int i) {
-		int j = 0;
-		ListElement<T> ptr = this.first;
+    @Override
+    public  void add(final T e) {
+        final ListElement<T> insert = new ListElement<>(e, null, this.first);
+        insert.lock.lock();
+        try {
+        	if (this.first != null) {
+   	            this.first.prev = insert;
+   	        }
+   	        this.first = insert;
+        
+        }finally {
+        	insert.lock.unlock();
+        }
+	     
+    }
 
-		while (j++ < i) {
-
-			ptr.next.lockfirst.lock();
-			ptr.lockfirst.lock();
-
-			try {
-
-				ptr = ptr.next;
-			} finally {
-				ptr.lockfirst.unlock();
-				ptr.prev.lockfirst.unlock();
-			}
-		}
-
-		return ptr.element;
-	}
-
-	@Override
-	public synchronized void add(final T e) {
-		final ListElement<T> insert = new ListElement<>(e, null, this.first);
-		if (this.first != null) {
-			this.first.prev = insert;
-		}
-		this.first = insert;
-		System.out.println(insert.element);
-	}
-
-	@Override
-	public void mod(final int i, final T e) {
-		int j = 0;
-		ListElement<T> ptr = this.first;
-	
-	
-		while (j++ < i) {
-			
-			ptr.lockfirst.lock();
-			ptr.next.lockfirst.lock();
-			
-			try {
-				
-			ptr = ptr.next;
-
-			}finally {
-				ptr.lockfirst.unlock();
-				ptr.prev.lockfirst.unlock();
-			}
-		}
-		ptr.element = e;
-		
-	}
+    @Override
+    public  void mod(final int i, final T e) {
+        int j = 0;
+        ListElement<T> ptr = this.first;
+        ListElement<T> tmp = ptr;
+        //Aktueller Node wird gelockt
+        ptr.lock.lock();
+        while (j++ < i) {
+        	
+        	try {
+            
+        	ptr = ptr.next;
+        	tmp = ptr.prev;
+        	//den nächsten Node locken
+        	ptr.lock.lock();
+            
+        	}
+        	finally {
+        		tmp.lock.unlock();
+        	}
+        	
+        }
+        
+        try {
+        tmp = ptr.next;
+        tmp.lock.lock();
+        ptr.element = e;
+        }
+        finally {
+        	tmp.lock.unlock();
+        	ptr.lock.unlock();
+        }
+        }
 
 }
